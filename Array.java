9514cfb5c0086a8f446eaee2c27478a812d74771
Ajin Saji <ajinsaji2004@gmail.com>
2025-09-16 15:13:07 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Array {
+	public static void main(String[]args) {
+		int [] array= new int[5];
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the elements:");
+		
+		for(int i = 0;i<5;i++) {
+			array[i]=sc.nextInt();
+			
+			
+		}
+	
+		}
+	}
+	
