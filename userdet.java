f8c835d74ee2f12269685a2d6794d1155ba337d6
Ajin Saji <ajinsaji2004@gmail.com>
2025-09-16 15:20:06 +0530

    Create userdet.java

---
+++ userdet.java
@@ -0,0 +1,25 @@
+package ajin;
+import java.util.Scanner;
+
+public class userdet {
+	public static void main( String [] args ) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter a number :");
+		int n1=sc.nextInt();
+		System.out.println("The number :"+n1);
+		
+		System.out.println("enter a number :");
+		int n2=sc.nextInt();
+		System.out.println("The number :"+n2);
+		
+		if(n1>n2)
+		{
+			System.out.println( " n1 is greater");
+		}
+		else {
+			System.out.println("n2 is greater ");
+	}}
+
+}
